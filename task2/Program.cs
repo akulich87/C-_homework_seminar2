5d9034e85c74a12b1639b87cd1b22f35478b57af
akulich87 <amgrupp_project@mail.ru>
2023-03-17 21:34:18 +0300

    add task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -5,9 +5,22 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-Console.WriteLine("Введите число ");
-int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = number.ToString().Length;
 
-int number2 = number1 % 10;
+if(count >= 3)
+{
+  while(number > 999)
+  {
+    number = number / 10;
+  }
+  int result = number % 10;
+  Console.WriteLine("Третья цифра: " + result);
 
-Console.WriteLine(number2);
+}
+
+else
+{
+  Console.WriteLine("Третьей цифры нет ");
+}
