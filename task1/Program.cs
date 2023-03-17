cfce50d5bdc77cb55346b9c2dce2478712db4be4
akulich87 <amgrupp_project@mail.ru>
2023-03-17 16:07:20 +0300

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число
+//  и на выходе показывает вторую цифру этого числа
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+int number2 = (number1 / 10) % 10;
+
+Console.WriteLine(number2);
